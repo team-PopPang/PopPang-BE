ec28c3b654b879d204bb8efa811d0cd9d4d8ce31
DoNalD-A <devsong42@gmail.com>
2025-09-19 14:55:58 +0900

    chore: initial commit

---
+++ src/main/java/com/poppang/be/PoppangBeApplication.java
@@ -0,0 +1,13 @@
+package com.poppang.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoppangBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoppangBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poppang/be/PoppangBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poppang.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoppangBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
