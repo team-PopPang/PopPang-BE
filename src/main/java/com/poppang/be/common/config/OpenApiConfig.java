d7f1d90956987b66553b423e8d55996450a06d08
DoNalD-A <devsong42@gmail.com>
2025-10-04 15:56:54 +0900

    chore: Swagger 설정 파일 추가

---
+++ src/main/java/com/poppang/be/common/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.poppang.be.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PopPang API 리스트")
+                        .description("Poppang 서비스 API 문서")
+                        .version("v1.0.0"));
+    }
+}
