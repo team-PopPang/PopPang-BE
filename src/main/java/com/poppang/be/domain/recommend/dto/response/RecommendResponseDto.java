4a74f88a1028d10de8b499fd4934031ae3819cc4
DoNalD-A <devsong42@gmail.com>
2025-10-08 14:18:01 +0900

    feat: 추천 리스트 전체 반환 기능 구현

---
+++ src/main/java/com/poppang/be/domain/recommend/dto/response/RecommendResponseDto.java
@@ -0,0 +1,30 @@
+package com.poppang.be.domain.recommend.dto.response;
+
+import com.poppang.be.domain.recommend.entity.Recommend;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RecommendResponseDto {
+
+    private Long id;
+    private String recommendName;
+
+    @Builder
+    public RecommendResponseDto(Long id,
+                                String recommendName) {
+        this.id = id;
+        this.recommendName = recommendName;
+
+    }
+
+    public static RecommendResponseDto from(Recommend recommend) {
+        return RecommendResponseDto.builder()
+                .id(recommend.getId())
+                .recommendName(recommend.getRecommendName())
+                .build();
+    }
+
+}
