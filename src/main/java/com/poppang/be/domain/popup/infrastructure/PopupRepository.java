65b58c0b113575077f53ea649bbb02a873c7ed6f
DoNalD-A <devsong42@gmail.com>
2025-10-08 14:18:13 +0900

    feat: 팝업 리스트 전체 반환 기능 구현

---
+++ src/main/java/com/poppang/be/domain/popup/infrastructure/PopupRepository.java
@@ -0,0 +1,7 @@
+package com.poppang.be.domain.popup.infrastructure;
+
+import com.poppang.be.domain.popup.entity.Popup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopupRepository extends JpaRepository<Popup, Long> {
+}
