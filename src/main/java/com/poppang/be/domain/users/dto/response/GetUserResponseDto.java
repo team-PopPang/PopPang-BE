5072e6cb0895a98ead419dc488a12e7b794be93a
DoNalD-A <devsong42@gmail.com>
2025-11-04 00:51:45 +0900

    feat: 유저 정보 반환 기능 개발

---
+++ src/main/java/com/poppang/be/domain/users/dto/response/GetUserResponseDto.java
@@ -0,0 +1,58 @@
+package com.poppang.be.domain.users.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.poppang.be.common.enums.Role;
+import com.poppang.be.domain.users.entity.Provider;
+import com.poppang.be.domain.users.entity.Users;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GetUserResponseDto {
+
+    private String uid;
+    private String userUuid;
+    private Provider provider;
+    private String email;
+    private String nickname;
+    private Role role;
+
+    @JsonProperty("isAlerted")
+    private boolean alerted;
+    private String fcmToken;
+
+    @Builder
+    public GetUserResponseDto(String uid,
+                              String userUuid,
+                              Provider provider,
+                              String email,
+                              String nickname,
+                              Role role,
+                              boolean alerted,
+                              String fcmToken) {
+        this.uid = uid;
+        this.userUuid = userUuid;
+        this.provider = provider;
+        this.email = email;
+        this.nickname = nickname;
+        this.role = role;
+        this.alerted = alerted;
+        this.fcmToken = fcmToken;
+    }
+
+    public static GetUserResponseDto from(Users user) {
+        return GetUserResponseDto.builder()
+                .uid(user.getUid())
+                .userUuid(user.getUuid())
+                .provider(user.getProvider())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .role(user.getRole())
+                .alerted(user.isAlerted())
+                .fcmToken(user.getFcmToken())
+                .build();
+    }
+
+}
