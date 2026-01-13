4a6ec2e13e41b3538af72db26abb5fb6f3455f7e
DoNalD-A <162666567+DoNalD-A@users.noreply.github.com>
2026-01-13 12:53:45 +0900

    feat: 지도 상단 Featured 추천 조회 기능

--- src/main/java/com/poppang/be/domain/recommend/infrastructure/RecommendRepository.java
+++ src/main/java/com/poppang/be/domain/recommend/infrastructure/RecommendRepository.java
@@ -4,5 +4,10 @@ import com.poppang.be.domain.recommend.entity.Recommend;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RecommendRepository extends JpaRepository<Recommend, Long> {}
+public interface RecommendRepository extends JpaRepository<Recommend, Long> {
+
+    List<Recommend> findAllByIdIn(List<Integer> featuredRecommendIds);
+}
